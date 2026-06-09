8f4f2d7b25f0a55648f7ee2d661a4614065c2a37
Tommaso Stentella <stentell@molgen.mpg.de>
2026-06-09 15:47:57 +0200

    setnepochs automatically at setinit

--- src/utils.jl
+++ src/utils.jl
@@ -363,7 +363,7 @@ end
 Set the initial vector of parameters for the optimization which takes the `FitOptions` object `fop`.
 """
 function setinit!(fop::FitOptions, init::AbstractVector{Float64})
-    @assert length(init) == npar(fop)
+    setnepochs!(fop, length(init) ÷ 2)
     fop.init .= init
     for i in eachindex(fop.init)
         fop.init[i] <= fop.low[i] ? fop.init[i] = fop.low[i] * 1.001 : nothing
